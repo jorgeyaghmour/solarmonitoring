bdfe27227199848e03ad310eba6dec8c979c5cbb
Juandi <morrincomero@outlook.com>
2022-10-02 15:10:51 -0400

    Added a comment to clarify what the route.js file is for

--- backend/api/solardb.route.js
+++ backend/api/solardb.route.js
@@ -1,8 +1,12 @@
+/**
+ * This file contains all the routes/URLs of the database.
+ * 
+ */
 import express from "express"
 
 const router = express.Router()
 
-// "/" signifies the root URL
+// "/" signifies the URL to the root directory
 router.route("/").get((req, res) => res.send("This is the root directory for the Solar Database"))
 
 // export statements allow this file to be imported by other files in the project
